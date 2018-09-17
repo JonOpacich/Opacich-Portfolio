16871766a3ea34434ab0fbc3e7973c75e091af89
Artaviia <jonopacich@gmail.com>
2018-09-17 17:26:10 -0500

    starbucks changes

--- my-app/src/pages/Portfolio/portfolio.jsx
+++ my-app/src/pages/Portfolio/portfolio.jsx
@@ -1,43 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
 
-let images = ["https://r.hswstatic.com/w_907/gif/tesla-cat.jpg", "https://www.readersdigest.ca/wp-content/uploads/sites/14/2011/01/4-ways-cheer-up-depressed-cat.jpg", "https://r.hswstatic.com/w_907/gif/tesla-cat.jpg", "https://www.readersdigest.ca/wp-content/uploads/sites/14/2011/01/4-ways-cheer-up-depressed-cat.jpg"]
+let images= ["http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"]
 
-export default class Portfolio extends React.Component {
-  render() {
-
-    const settings = {
-      customPaging: function(i) {
-        return (
-          <a className="thumb-container">
-            <img src={images[i]} className="thumb-img"/>
-          </a>
-        );
-      },
-      dots: true,
-      dotsClass: "slick-dots slick-thumb",
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      lazyLoad:"ondemand"
+export default class AsNavFor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nav1: null,
+      nav2: null
     };
+  }
 
+  componentDidMount() {
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2
+    });
+  }
+
+  render() {
     return (
       <div className="section">
-        <h2>Portfolio</h2>
-        <Slider {...settings}>
+        <Slider
+        className="horizontal"
+          asNavFor={this.state.nav2}
+          ref={slider => (this.slider1 = slider)}
+        >
           <div>
-            <img src={images[0]} />
+            <img src={images[0]} alt="" className="horizontal-img"/>
           </div>
           <div>
-            <img src={images[1]} />
+          <img src={images[0]} alt="" className="horizontal-img"/>
           </div>
           <div>
-            <img src={images[2]} />
+          <img src={images[0]} alt="" className="horizontal-img"/>
           </div>
           <div>
-            <img src={images[3]} />
+          <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+          <div>
+          <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+          <div>
+          <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+        </Slider>
+        <Slider
+          asNavFor={this.state.nav1}
+          ref={slider => (this.slider2 = slider)}
+          slidesToShow={3}
+          swipeToSlide={true}
+          focusOnSelect={true}
+          vertical={true}
+          verticalSwiping={true}
+        >
+          <div>
+            <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+          <div>
+          <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+          <div>
+          <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+          <div>
+          <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+          <div>
+          <img src={images[0]} alt="" className="horizontal-img"/>
+          </div>
+          <div>
+          <img src={images[0]} alt="" className="horizontal-img"/>
           </div>
         </Slider>
       </div>
