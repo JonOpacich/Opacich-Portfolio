f32e52893afdcad169bfaa2ac7d1157e11f6193e
jon Opacich <jonopacich@gmail.com>
2018-09-17 22:46:19 -0500

    monday changes

--- my-app/src/pages/About/about.jsx
+++ my-app/src/pages/About/about.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 
-let styles={
-  
-}
-
 const About = ({ mobile }) => {
   return (
-    <div className="section" style={styles}>
-      <div className="columns">
-      <div className="column">
-      column 1
-      </div>
-      <div className="column">
-      column 2
-      </div>
+    <div className="section">
+      <p className="section-title">
+        About <span class="section-logo">_</span>
+      </p>
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+              <img
+                src="http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"
+                alt="Personal Headshot"
+                className="headshot"
+              />
+            <p className="bio">
+              {" "}
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic aut
+              accusantium, exercitationem nulla animi commodi tempora dolores
+              tenetur, odit maxime ipsum at ex voluptas quia delectus
+              consequatur dolore earum suscipit.
+            </p>
+          </div>
+          <div className="column">column 2</div>
+        </div>
       </div>
     </div>
   );
--- my-app/src/pages/Portfolio/portfolio.jsx
+++ my-app/src/pages/Portfolio/portfolio.jsx
@@ -22,6 +22,7 @@ export default class AsNavFor extends Component {
   render() {
     return (
       <div className="section">
+        <p className="section-title">Projects <span class="section-logo">_</span></p>
         <Slider
         className="horizontal"
           asNavFor={this.state.nav2}
@@ -49,29 +50,31 @@ export default class AsNavFor extends Component {
         <Slider
           asNavFor={this.state.nav1}
           ref={slider => (this.slider2 = slider)}
-          slidesToShow={3}
+          slidesToShow={5}
           swipeToSlide={true}
           focusOnSelect={true}
           vertical={true}
           verticalSwiping={true}
+          speed={700}
+          arrows={false}
         >
           <div>
-            <img src={images[0]} alt="" className="horizontal-img"/>
+            <img src={images[0]} alt="" className="vertical-img"/>
           </div>
           <div>
-          <img src={images[0]} alt="" className="horizontal-img"/>
+          <img src={images[0]} alt="" className="vertical-img"/>
           </div>
           <div>
-          <img src={images[0]} alt="" className="horizontal-img"/>
+          <img src={images[0]} alt="" className="vertical-img"/>
           </div>
           <div>
-          <img src={images[0]} alt="" className="horizontal-img"/>
+          <img src={images[0]} alt="" className="vertical-img"/>
           </div>
           <div>
-          <img src={images[0]} alt="" className="horizontal-img"/>
+          <img src={images[0]} alt="" className="vertical-img"/>
           </div>
           <div>
-          <img src={images[0]} alt="" className="horizontal-img"/>
+          <img src={images[0]} alt="" className="vertical-img"/>
           </div>
         </Slider>
       </div>
